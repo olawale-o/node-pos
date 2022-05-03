f2d41f2acb22e75e3ecf52a31a8950eebe1a52c1
olawale-o <omoogunolawale@gmail.com>
2022-05-03 11:02:39 +0100

    Modify server

--- user/index.js
+++ user/index.js
@@ -1,9 +1,11 @@
+const  env = require('./constants');
 const http = require('http');
+const { PORT } = env;
 
 const app = require('./config/app');
 const server = http.createServer(app);
 
-server.listen(5001, () => {
+server.listen(PORT || 5001, () => {
   console.log('Server started on port 5001');
 });
 
