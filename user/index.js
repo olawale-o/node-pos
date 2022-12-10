c6e7c6d5326a39017c8ec7238fa6dec5f60ac5d9
olawale-o <omoogunolawale@gmail.com>
2022-12-10 16:52:40 +0100

    Create app

--- user/index.js
+++ user/index.js
@@ -4,9 +4,28 @@ const { PORT } = env;
 
 const app = require('./config/app');
 const server = http.createServer(app);
+const { Server } = require('socket.io');
+const { MongoClient } = require('mongodb');
+
+const dbConnection = require('./database/connection');
+const socketConnection = require('./socketConnection');
+
+const IO = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000"
+  }
+});
+
+socketConnection(IO);
+dbConnection(MongoClient)
+.then((result) => {
+  console.log(result);
+}).catch((err) => {
+  console.log(err)
+})
 
 server.listen(PORT || 5001, () => {
   console.log('Server started on port 5001');
 });
 
-module.exports = server;
+module.exports = server; 
