3b29420d42b13368b45136fff18a76c3e8d3dac9
olawale-o <omoogunolawale@gmail.com>
2022-12-10 20:45:38 +0100

    Listen to server

---
+++ connection/index.js
@@ -0,0 +1,20 @@
+const  env = require('./constants');
+const { PORT } = env;
+
+const app = require('./config/app');
+const { MongoClient } = require('mongodb');
+
+const dbConnection = require('./database/connection');
+
+dbConnection(MongoClient)
+.then((result) => {
+  console.log(result);
+}).catch((err) => {
+  console.log(err)
+})
+
+app.listen(PORT || 5002, () => {
+  console.log('Server started on port 5001');
+});
+
+module.exports = app; 
