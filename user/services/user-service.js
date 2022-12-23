6a273b7f702596b2c78c202a30b0f91885151521
olawale-o <omoogunolawale@gmail.com>
2022-12-23 02:56:49 +0100

    Create event

--- user/services/user-service.js
+++ user/services/user-service.js
@@ -2,8 +2,23 @@ const userRepository = require('../repository/user-repository');
 
 module.exports = {
   register: async (crendentials) => await userRepository.create(crendentials),
-  verifyUserName: async (crendentials) => await userRepository.findByUsername(crendentials),
-  verifyPassword: async ({password, passwordEncrypt}) => {
-    return password === passwordEncrypt;
-  } 
+  login: async (credentials) => {
+    const user = this._verifyUserName(credentials);
+    if (this._verifyPassword({ password: user.password, passwordEncrypt: 'encrypt' })) {
+      throw new Error("Password invalid")
+    }
+    return user;
+  },
+  _verifyUserName: async (crendentials) => await userRepository.findByUsername(crendentials),
+  _verifyPassword: async ({ password, passwordEncrypt }) => password === passwordEncrypt,
+  subscribeEvents: async (payload) => {
+    const { event, data } = payload;
+    switch (event) {
+      case 'TEST':
+        console.log('it is working', data);
+        break;
+      default:
+        break;
+    }
+  }
 }
