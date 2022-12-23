0a0c5ee45be9b2bdf6d61788c5a8b182a57b74d4
olawale-o <omoogunolawale@gmail.com>
2022-12-23 02:57:29 +0100

    Use appEvent

--- user/config/app.js
+++ user/config/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const errorHandler = require('../middleware/errorHandler');
+const appEvent = require('../events/app-event');
 
 const app = express();
 
@@ -9,6 +10,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
+
+appEvent(app);
 
 app.use('/v1', require('../route'));
 
