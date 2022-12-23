94be15668fa3e7cf235c2a280f9e33088cc49b51
olawale-o <omoogunolawale@gmail.com>
2022-12-23 02:58:08 +0100

    Modify files

--- user/controller.js
+++ user/controller.js
@@ -15,8 +15,18 @@ module.exports = {
   login: async (req, res) => {
     const { username, password } = req.body;
     try {
-      const user = await userService.verifyUserName({ username });
-      await userService.verifyPassword(password, 'encrypt');
+      const user = await userService.login({ username, password });
+      return res.status(201).json(user);  
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  createUser: async (req, res) => {
+    const { username, name } = req.body;
+    try {
+      const user = await userService.register(
+        { username, name, createdAt: new Date(), updatedAt: new Date() }
+      );
       return res.status(201).json(user);  
     } catch (error) {
       console.log(error);
--- user/route.js
+++ user/route.js
@@ -3,5 +3,6 @@ const controller = require('./controller');
 
 router.post('/register', controller.create);
 router.post('/login', controller.login);
+router.post('/new', controller.createUser);
 
 module.exports = router;
