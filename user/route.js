00ffea567a99c1bc890f3d33a95c86d0a1bc4ef5
olawale-o <omoogunolawale@gmail.com>
2022-12-23 01:55:43 +0100

    Modify user route

--- user/route.js
+++ user/route.js
@@ -1,24 +1,7 @@
-const { env: { LOCAL_MONGODB_SINGLESET } } = require('./constants');
 const router = require('express').Router();
-const { ObjectID } = require('bson');
-const { MongoClient } = require('mongodb');
+const controller = require('./controller');
 
-const client = new MongoClient(LOCAL_MONGODB_SINGLESET);
+router.post('/register', controller.create);
+router.post('/login', controller.login);
 
-const User = client.db('socialdb').collection('users');
-
-router.post('/', async (req, res, next) => {
-  const { name, username } = req.body;
-  console.log(name)
-  try {
-    const newUser = { name, username, createdAt: new Date(), updatedAt: new Date(), };
-    const userId = await User.insertOne(newUser);
-    return res.status(200).json({
-      ...newUser,
-      id: userId,
-    })
-  } catch (error) {
-    console.log(error);
-  }  
-});
 module.exports = router;
