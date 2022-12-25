62ac6dd5c8006cc9bdf6c3716a4d29ddd0a49d58
olawale-o <omoogunolawale@gmail.com>
2022-12-25 05:29:17 +0100

    Add more route

--- connection/route.js
+++ connection/route.js
@@ -1,34 +1,7 @@
-const { env: { LOCAL_MONGODB_SINGLESET } } = require('./constants');
-
 module.exports = function(socket) {
   const router = require('express').Router();
-  const { ObjectID } = require('bson');
-  const { MongoClient } = require('mongodb');
-  
-  const client = new MongoClient(LOCAL_MONGODB_SINGLESET);
-  
-  const Friend = client.db('socialdb').collection('friends');
-  router.post('/', async (req, res, next) => {
-    const { requester, recipient } = req.body;
-    try {
-      const newFriend = {
-        users: [requester, recipient],
-        status: 'request',
-        request: {
-          from: requester,
-          to: recipient,
-        },
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      };
-      const userId = await Friend.insertOne(newFriend);
-      return res.status(200).json({
-        ...newFriend,
-        id: userId,
-      })
-    } catch (error) {
-      console.log(error);
-    }  
-  });
+  const controller = require('./controller');
+
+  router.post('/friend-requests', controller.friendRequest);
   return router;
 };
