406e220935bdf2e8fc47ba6d6842e4932750f659
olawale-o <omoogunolawale@gmail.com>
2022-12-25 05:30:20 +0100

    Add more actions to controller

--- user/controller.js
+++ user/controller.js
@@ -1,4 +1,5 @@
 const userService = require('./services/user-service');
+const publisher = require('./events/publisher');
 
 module.exports = {
   create: async (req, res) => {
@@ -28,6 +29,16 @@ module.exports = {
         { username, name, createdAt: new Date(), updatedAt: new Date() }
       );
       return res.status(201).json(user);  
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  myFriendRequest: async (req, res) => {
+    const { userId } = req.body;
+    try {
+      const payload = await userService.getPendingConnectionPayload({ userId }, 'GET_PENDING_REQUEST');
+      await publisher.PublishConnectionEvent(payload);
+      return res.status(201).json(payload);  
     } catch (error) {
       console.log(error);
     }
