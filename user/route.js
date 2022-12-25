f64ca3dccc6bd056bfed9e1ef57d2cb6f4fa4890
olawale-o <omoogunolawale@gmail.com>
2022-12-25 05:30:34 +0100

    Add more route

--- user/route.js
+++ user/route.js
@@ -4,5 +4,6 @@ const controller = require('./controller');
 router.post('/register', controller.create);
 router.post('/login', controller.login);
 router.post('/new', controller.createUser);
+router.post('/friend-requests', controller.myFriendRequest);
 
 module.exports = router;
