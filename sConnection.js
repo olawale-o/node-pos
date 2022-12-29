7f3fbebf6317df5b77bcbaf3d6fcd47e4f157e40
olawale-o <omoogunolawale@gmail.com>
2022-12-29 08:45:10 +0100

    Fix chat messages bug

--- sConnection.js
+++ sConnection.js
@@ -5,6 +5,7 @@ const { ObjectID } = require('bson');
 const client = new MongoClient(LOCAL_MONGODB_SINGLESET);
 
 const User = client.db('socialdb').collection('users');
+const Conversation = client.db('socialdb').collection('conversations');
 
 let onlineUsers = {};
 
@@ -32,29 +33,29 @@ const findMessagesForUser = (userId) => {
 }
 
 const getMessagesForUser = (userId) => {
-  const messagesForsUser = new Map();
+  const messagesPerUser = new Map();
   findMessagesForUser(userId).forEach((message) => {
     const { from, to } = message;
     const otherUser = userId === from ? to : from;
-    if (messagesForsUser.has(otherUser)) {
-      messagesForsUser.get(otherUser).push(message);
+    if (messagesPerUser.has(otherUser)) {
+      messagesPerUser.get(otherUser).push(message);
     } else {
-      messagesForsUser.set(otherUser, [message])
+      messagesPerUser.set(otherUser, [message])
     }
-  })
+  });
+  return messagesPerUser;
 }
-
 
 module.exports = function(IO) {
   IO.use(async (socket, next) => {
     const sessionId = socket.handshake.auth.sessionId;
     if (sessionId) {
       const session = await findSession(sessionId);
-      console.log('session', session)
       if (session) {
         socket.sessionId = sessionId;
         socket.userId = session.userId
         socket.username = session.username;
+        socket._id = session._id;
         return next();
       } else {
         return next(new Error("Invalid session"))
@@ -67,23 +68,25 @@ module.exports = function(IO) {
     socket.username = user.username;
     socket.userId = user._id;
     socket.sessionId = user._id;
+    socket._id = user._id;
     next()
   })
 
   IO.on('connection', async (socket) => {
     saveSession(socket.sessionId, {
-      userId: socket.userId, username: socket.username,
-      connected: true,
+      userId: socket.userId, username: socket.username, _id: socket._id,
+      online: true,
     })
     await socket.join(socket.userId);
     const users = [];
+    const userMessages = getMessagesForUser(socket.userId)
     findSessions().forEach((session) => {
       if (session.userId !== socket.userId) {
         users.push({
           userId: session.userId,
           username: session.username,
           connected: session.connected,
-          // messages: userMessages.get(session.userId) || [],
+          messages: userMessages.get(session.userId) || [],
         })
       }
     })
@@ -100,30 +103,43 @@ module.exports = function(IO) {
       userId: socket.userId,
       username: socket.username,
       sessionId: socket.sessionId,
+      _id: socket._id,
     });
 
-    socket.on('private message', ({ content, to }) => {
+    socket.on('private message', async ({ text, to }) => {
       const newMessage = {
         from: socket.userId,
         to,
-        content
+        text,
+        username: socket.username
       }
+      console.log('new private message')
+      await Conversation.insertOne({
+        from: socket.userId,
+        to,
+        text,
+        createdAt: new Date(Date.now()),
+        updatedAt: new Date(Date.now()),
+      });
       socket.to(to).emit("private message", newMessage);
       saveMessages(newMessage);
-    })
+    });
 
-    socket.on('new message', (message) => {
-      socket.broadcast.emit('new message', {
-        userId: socket.userId,
-        username: socket.username,
-        message,
+    socket.on('user messages', ({ _id, username }) => {
+      // pull messages from database
+      const userMessages = getMessagesForUser(socket._id);
+      console.log(userMessages);
+      console.log(userMessages.get(_id));
+      socket.emit('user messages', {
+        userId: _id,
+        _id,
+        username,
+        messages: userMessages.get(_id) || []
       })
     });
 
     socket.on('disconnect', async () => {
-      console.log('disconnect' ,socket.userId)
       const matchingSockets = await IO.in(socket.userId).allSockets();
-      console.log('matchingSockets', matchingSockets);
       const isDisconnected = matchingSockets.size === 0;
       if (isDisconnected) {
         await User.findOneAndUpdate({ _id: ObjectID(socket.userId) }, { $set: { online: false } })
