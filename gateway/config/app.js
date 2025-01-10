9268623022dd3711b86510ceee9db4b962ab722f
olawale-o <omoogunolawale@gmail.com>
2025-01-10 05:07:54 +0100

    Add products endpoint

--- gateway/config/app.js
+++ gateway/config/app.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-const proxy = require('express-http-proxy');
+const express = require("express");
+const cors = require("cors");
+const proxy = require("express-http-proxy");
 
 const app = express();
 
@@ -8,6 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.use('/api/user', proxy('http://localhost:5001'));
+app.use("/api/users", proxy("http://localhost:5001"));
+app.use("/api/products", proxy("http://localhost:5002"));
 
 module.exports = app;
