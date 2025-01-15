ce55a9372c396f432d4155d55898ba5be4a41931
olawale-o <omoogunolawale@gmail.com>
2025-01-15 15:22:29 +0100

    Listen to incomming event

--- products/api/v1/app-event.js
+++ products/api/v1/app-event.js
@@ -1,6 +1,10 @@
+const event = require("./event");
+
 const router = require("express").Router();
 
-router.use("/", async (req, res, next) => {
+router.use("/", async (req, res) => {
+  const { payload } = req.body;
+  await event.consumeEvent(payload);
   res.status(200).json({ status: true });
 });
 
