41c6107ca20e0690459fabf203d6211024098c21
olawale-o <omoogunolawale@gmail.com>
2025-01-16 17:59:32 +0100

    Create user model

---
+++ products/models/user.js
@@ -0,0 +1,55 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+
+      // User.hasMany(models.Product_Subscription, {
+      //   foreignKey: "user_id",
+      // });
+      User.belongsToMany(models.Product, {
+        through: models.Product_Subscription,
+        as: "my_subscriptions",
+        foreignKey: "user_id",
+      });
+    }
+  }
+  User.init(
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+      tableName: "users",
+      updatedAt: false,
+      createdAt: "created_at",
+    },
+  );
+
+  return User;
+};
