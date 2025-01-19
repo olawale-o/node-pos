9037b76ef15e973acc0d06636b3cf0e694ac320c
olawale-o <omoogunolawale@gmail.com>
2025-01-19 15:03:59 +0100

    Update handler

--- products/api/v1/handler/index.js
+++ products/api/v1/handler/index.js
@@ -17,23 +17,35 @@ module.exports = {
     try {
       const { productId } = req.params;
       const data = req.body;
+
       const product = await service.subscribe({
         productId,
-        userId: data.userId,
+        user_id: data.userId,
       });
       if (!product) {
         throw new Error("Unable to subscribe to product");
       }
-      const payload = await service.getProductPayload(
-        productId,
-        data.userId,
-        "PRODUCT_SUBSCRIPTION",
-      );
-      await publishUserEvent(payload);
+      // const payload = await service.getProductPayload(
+      //   productId,
+      //   data.userId,
+      //   "PRODUCT_SUBSCRIPTION",
+      // );
+      // await publishUserEvent(payload);
       res.status(200).json({ message: "Thank you for subscribing" });
     } catch (e) {
       console.log(e);
       res.status(500).json({ message: "Internal Server error" });
+    }
+  },
+  update: async (req, res, next) => {
+    try {
+      const { productId } = req.params;
+      const data = req.body;
+      await service.update({ productId, data });
+      res.status(200).json({ message: "Product updated" });
+    } catch (e) {
+      console.log(e);
+      res.status(500).json({ message: "Internal server error" });
     }
   },
 };
