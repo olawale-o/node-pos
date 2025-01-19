c023aa4a2913b730c4909426abac374ce60ca363
olawale-o <omoogunolawale@gmail.com>
2025-01-19 15:02:57 +0100

    Update productNotificationMessage

--- products/api/v1/jobs/bull/job.js
+++ products/api/v1/jobs/bull/job.js
@@ -1,11 +1,31 @@
+const db = require("../../../../models");
+
 const welcomeMessage = (job) => {
   console.log("Sending a welcome message every few seconds");
 };
 
-const productNotificationMessage = (job) => {
-  console.log(
-    "Sending a product notification message to " + job.data.jobData.email,
-  );
+const productNotificationMessage = async (job) => {
+  const sendMail = (index, proudct, email) => {
+    console.log(
+      index + " Sending product notification " + proudct + " to " + email,
+    );
+  };
+  const { jobData } = job.data;
+
+  const rows = await db.ProductSubscription.findAll({
+    where: { productId: jobData.productId },
+    include: {
+      as: "product_subscribers",
+      model: db.User,
+    },
+  });
+  rows.forEach((row, index) => {
+    sendMail(
+      index,
+      row.dataValues.productId,
+      row.dataValues.product_subscribers.email,
+    );
+  });
 };
 
 module.exports = { welcomeMessage, productNotificationMessage };
