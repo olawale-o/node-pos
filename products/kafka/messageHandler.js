e84101b10916def95cfbea3692f5bdf42978e5da
olawale-o <omoogunolawale@gmail.com>
2025-01-25 05:02:45 +0100

    Implement message handler logic

---
+++ products/kafka/messageHandler.js
@@ -0,0 +1,33 @@
+const { emailQueue } = require("../api/v1/jobs/bull/queue");
+const { addJob } = require("../api/v1/jobs/bull");
+const handleMessage =
+  (consumer) =>
+  async ({ topic, partition, message }) => {
+    const data = JSON.parse(message.value);
+    console.log(
+      `Received message from topic '${topic}': '${partition}': ${data.toString()}`,
+    );
+
+    if (topic === "email-topic") {
+      console.log("Handling email notification:", message.value.toString());
+      await addJob(
+        emailQueue,
+        {
+          name: "productNotificationMessage",
+          jobData: {
+            productId: data.productId,
+          },
+        },
+        { removeOnComplete: true },
+      );
+    } else if (topic === "sms-topic") {
+      console.log("Handling SMS notification:", message.value.toString());
+    } else {
+      console.log("Unknown topic:", topic);
+    }
+    await consumer.commitOffsets([
+      { topic, partition, offset: message.offset },
+    ]);
+  };
+
+module.exports = { handleMessage };
